70ab480dbbdcd4b1091803dc11d715336b5a42b1
bmartinson <brian@brianmartinson.com>
2019-08-19 23:17:40 -0700

    Project Maintenance

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe("workspace-project App", () => {
 
   it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual("Welcome to NgxXBrowserSVGMaskLibrary!");
+    expect(page.getTitleText()).toEqual("Welcome to ngx-x-browser-svg-mask!");
   });
 
   afterEach(async () => {
