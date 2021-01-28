be446987cdb5ba676c673e3f5a398d4c24e02d3b
bmartinson <brian@brianmartinson.com>
2021-01-27 18:57:57 -0800

    Return Example Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxXBrowserSVGMaskModule } from '../../projects/ngx-x-browser-svg-mask/src/lib/ngx-x-browser-svg-mask.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxXBrowserSVGMaskModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
