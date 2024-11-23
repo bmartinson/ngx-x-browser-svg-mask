81a473b803fb3aae5a599deb63507d33fbcb385b
bmartinson <brian@brianmartinson.com>
2024-11-23 13:40:17 -0800

    Package Updates

--- projects/ngx-x-browser-svg-mask/src/lib/directive/ngx-x-browser-svg-mask.directive.ts
+++ projects/ngx-x-browser-svg-mask/src/lib/directive/ngx-x-browser-svg-mask.directive.ts
@@ -1,7 +1,8 @@
 import { Directive, ElementRef, HostBinding, Input } from '@angular/core';
 
 @Directive({
-  selector: '[ngxSVGMask]',
+    selector: '[ngxSVGMask]',
+    standalone: false
 })
 export class NgxXBrowserSVGMaskDirective {
 
